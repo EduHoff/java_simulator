7ad564d84798baf994583897ae98edb92c2ca58b
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-06-02 09:33:55 -0300

    add classe pessoa, nada realmente significante ainda

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,22 @@
 package application;
 
+import entities.Person;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Não a nada para ver por enquanto, por favor volte quando eu tiver programado mais! :D");
+		Person ps1 = new Person("Eduardo",18);
+		
+		System.out.println("Nome: " + ps1.getName() + ";" );
+		System.out.println("Idade: " + ps1.getAge() + ";" );
+		
+		Person ps2 = new Person();
+		ps2.setName("Edward");
+		ps2.setAge(19);
+		
+		System.out.println("Nome: " + ps2.getName() + ";" );
+		System.out.println("Idade: " + ps2.getAge() + ";" );
+		
 
 	}
 
